6909ddc3add2f9cf14c7af2cb5d571a7902dbe3b
Craig <craig@oppkey.com>
2023-03-19 08:43:45 -0700

    Revert "02_joystick"
    This reverts commit ac24b459d180fc860072dc5437856633e2873915.

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'dart:async';
 
-import 'package:boboo_tutorial/actors/character_component.dart';
-import 'package:boboo_tutorial/world/background_component.dart';
-import 'package:boboo_tutorial/world/game_joystick.dart';
+import 'package:flame/components.dart';
 import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flutter/material.dart';
@@ -14,23 +12,32 @@ main() {
   runApp(GameWidget(game: BobooGame()));
 }
 
-class BobooGame extends FlameGame with HasDraggables {
-  late final GameJoystick joystick;
-  CharacterComponent boboo = CharacterComponent();
-  BackgroundComponent kitchen = BackgroundComponent();
+class BobooGame extends FlameGame {
+  SpriteComponent boboo = SpriteComponent();
+  SpriteComponent kitchen = SpriteComponent();
 
   @override
   FutureOr<void> onLoad() async {
-    add(kitchen);
-    add(boboo);
-    joystick = GameJoystick();
-    add(joystick);
+    add(
+      kitchen
+        ..sprite = await loadSprite('world/kitchen.png')
+        ..size = size,
+    );
+    add(
+      boboo
+        ..sprite = await loadSprite('actors/boboo_solo.png')
+        ..size = Vector2.all(138)
+        ..position = Vector2(0, 200)
+        ..anchor = Anchor.center,
+    );
     return super.onLoad();
   }
 
   @override
   void update(double dt) {
-    // debugPrint(joystick.direction.toString());
+    if (boboo.x < size.x) {
+      boboo.x += 60 * dt;
+    }
 
     super.update(dt);
   }
